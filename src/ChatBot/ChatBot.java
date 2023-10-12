7b044a79c7e817264fe365168904c050510413d7
unknown <v.o.panasovskyi@student.khai.edu>
2023-10-12 16:24:22 +0300

    2-et

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,6 +1,7 @@
 package ChatBot;
 
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
@@ -11,5 +12,24 @@ public class ChatBot {
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + currentYear + ".");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, remind me your name.");
+        String yourName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + yourName + "!");
+
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5, and 7 in separate lines.");
+
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        int yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is " + yourAge + "; that's a good time to start programming!");
+
+        scanner.close();
     }
 }
