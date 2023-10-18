d7e188abcef8e3f18a28b412cb2ced1f53121b62
unknown <v.o.panasovskyi@student.khai.edu>
2023-10-18 14:00:35 +0300

    3-Ha

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,25 +1,31 @@
 package Hangman;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
 
+        String[] words = {"python", "java", "javascript", "kotlin"};
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length);
+        String secretWord = words[randomIndex];
+
         System.out.println("HANGMAN");
         System.out.println("Guess the word: ");
-
-        String secretWord = "java";
 
         Scanner scanner = new Scanner(System.in);
 
         String playerGuess = scanner.next();
-
 
         if (playerGuess.equals(secretWord)) {
             System.out.println("You survived!");
         } else {
             System.out.println("You lost!");
         }
+
+        System.out.println("The correct word was: " + secretWord);
 
         scanner.close();
     }
