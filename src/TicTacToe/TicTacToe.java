590d55629cf0987c97fe9f6c0f3f494ddff3bc74
unknown <v.o.panasovskyi@student.khai.edu>
2023-11-13 14:26:06 +0200

    TicTacToe-2

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,23 +1,58 @@
 package TicTacToe;
+
+import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // Ігрове поле 3x3
         char[][] gameBoard = {
-                {'X', 'O', 'X'},
-                {'O', 'X', 'O'},
-                {'X', 'O', 'X'}
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
         };
 
-        // Друк ігрового поля
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+
+        if (input.length() != 9) {
+            System.out.println("Invalid input. Please enter exactly 9 characters.");
+            return;
+        }
+
+        fillGameBoard(gameBoard, input);
+
+        // Друкуємо ігрове поле
         printGameBoard(gameBoard);
     }
 
+    // Метод для друкування ігрового поля
     public static void printGameBoard(char[][] gameBoard) {
+        System.out.println("---------");
         for (int row = 0; row < gameBoard.length; row++) {
+            System.out.print("| ");
             for (int col = 0; col < gameBoard[row].length; col++) {
                 System.out.print(gameBoard[row][col] + " ");
             }
-            System.out.println();
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+
+    // Метод для заповнення ігрового поля на основі введеного рядка
+    public static void fillGameBoard(char[][] gameBoard, String input) {
+        int index = 0;
+        for (int row = 0; row < gameBoard.length; row++) {
+            for (int col = 0; col < gameBoard[row].length; col++) {
+                char currentChar = input.charAt(index);
+                if (currentChar == 'X' || currentChar == 'O' || currentChar == '_') {
+                    gameBoard[row][col] = currentChar;
+                } else {
+                    System.out.println("Invalid character detected: " + currentChar);
+                    return;
+                }
+                index++;
+            }
         }
     }
 }
